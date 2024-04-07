c3cc20c858ee75a7c9019d710da46f3e3c6355e1
Bahiakhkh <bahiakhkh1@gmail.com>
2024-04-07 21:30:11 +0200

    changement hello world

--- hello.java
+++ hello.java
@@ -1,5 +1,19 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class Greeting {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Créer un objet Scanner pour lire l'entrée de l'utilisateur
+        Scanner scanner = new Scanner(System.in);
+        
+        // Demander à l'utilisateur son nom
+        System.out.print("Entrez votre nom : ");
+        String name = scanner.nextLine();
+        
+        // Afficher un message de salutation
+        System.out.println("Bonjour, " + name + " !");
+        
+        // Fermer le scanner
+        scanner.close();
     }
 }
+
