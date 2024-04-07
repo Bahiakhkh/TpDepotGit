9310c774a75636e9b438b6c10c47b556f69d27b5
Bahiakhkh <bahiakhkh1@gmail.com>
2024-04-08 00:40:36 +0200

    changement sur bug fix

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello , how are you doing?");
+        System.out.println("Hello  sir , how are you doing?");
     }
 }
